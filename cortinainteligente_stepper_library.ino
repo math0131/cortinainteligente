cd271646ddaf8e05efb5d4cf192b9303f4a1fe56
Matheus A. Martins <73441307+math0131@users.noreply.github.com>
2021-07-30 04:24:31 -0300

    Uso da biblioteca "Stepper.h"
    Controle mais apurado do motor, baseado em valores numéricos e não na alternância do estado.

---
+++ cortinainteligente_stepper_library.ino
@@ -0,0 +1,136 @@
+#include <Stepper.h>
+
+/*
+ * This is a basic example on how to use Espalexa and its device declaration methods.
+ */ 
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+#include <Espalexa.h>
+
+
+
+// prototypes
+boolean connectWifi();
+
+//callback functions
+void refCortina(uint8_t brightness);
+
+
+// Change this!!
+const char* ssid = "Martins";
+const char* password = "matheus2007";
+
+boolean wifiConnected = false;
+
+
+const int stepsPerRevolution = 800;
+
+Stepper myStepper(stepsPerRevolution, D1, D2);
+
+
+Espalexa espalexa;
+
+//Variavel para posição da cortina
+double pos = 0;
+
+//Variavel para valor de acrecimo: Tamanho da cortina/Tamanho dos steps = Steps totais, Valor Requerido(0-255 do Espalexa)/Steps totais = Acrecimo final
+const double acr = 0.159375;
+
+
+
+
+
+void setup()
+{
+  myStepper.setSpeed(60);
+
+ 
+
+  
+  Serial.begin(115200);
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+  
+  if(wifiConnected){
+    
+    // Define your devices here. 
+    espalexa.addDevice("Cortina", refCortina, 0); //simplest definition, default state off
+    
+    
+   
+
+
+    espalexa.begin();
+    
+  } else
+  {
+    while (1) {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
+  }
+
+  
+}
+ 
+void loop()
+{
+   espalexa.loop();
+   delay(1);
+}
+
+//our callback functions
+void refCortina(uint8_t brightness) {
+
+    for(pos > brightness; (pos += acr) < brightness; pos += acr){
+      Serial.println("indo");
+      myStepper.step(1);
+       
+    }
+
+    for(pos < brightness; (pos -= acr) > brightness; pos -= acr){
+      Serial.println("voltando");
+      myStepper.step(-1);
+      
+      
+       
+    }
+}
+
+
+
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi(){
+  boolean state = true;
+  int i = 0;
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20){
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state){
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
